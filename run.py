4fb214535e778236891551393fa60bc85dc58737
Cosmin Poieana <cmin764@gmail.com>
2016-06-07 12:30:29 +0300

    Deploy heroku

--- run.py
+++ run.py
@@ -1,12 +1,7 @@
 #! /usr/bin/env python
 
-import sys
-
 from coliw import coliw
 
 
-port = 1337
-if len(sys.argv) > 1:
-    port = int(sys.argv[1])
-
-coliw.run(host="0.0.0.0", port=port, threaded=True)
+if __name__ == "__main__":
+    coliw.run(host="0.0.0.0", port=1337, threaded=True)
